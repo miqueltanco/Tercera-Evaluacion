5d18ef3c6e9e3c8fc2f01f2226878ae4ffddac5c
alumne-DAM <alumne-DAM@EquipoSMX>
2019-04-05 18:16:03 +0200

    EEEE

--- Tercera Evaluacion/Marzo/Arkanoid/Arkanoid.java
+++ Tercera Evaluacion/Marzo/Arkanoid/Arkanoid.java
@@ -38,8 +38,8 @@ public class Arkanoid extends JFrame implements KeyListener {
 	public static final double ANCHURA_BLOQUE = 60.0; //60.0
 	public static final double ALTURA_BLOQUE = 20.0; //20.0
 
-	public static final int CANTIDAD_BLOQUES_X = 2; //11
-	public static final int CANTIDAD_BLOQUES_Y = 1; //4
+	public static int CANTIDAD_BLOQUES_X = 11; //11
+	public static int CANTIDAD_BLOQUES_Y = 5; //5
 
 	public static final int VIDAS_JUGADOR = 5; //5
 
@@ -52,6 +52,7 @@ public class Arkanoid extends JFrame implements KeyListener {
 
 	public static final String SONIDOMUERTE = ".\\bin\\Arkanoid\\Items\\sonidomuerte.mp3";
 	public static final String VICTORIA = ".\\bin\\Arkanoid\\Items\\victoria.mp3";
+	public static final String AMBIENTE = ".\\bin\\Arkanoid\\Items\\ambiente.wav";
 
 	public static final String BACKGROUND = "\\Items\\imagen 3.png";
 	public static final String ICONO = "\\Items\\icono.png";
@@ -67,13 +68,18 @@ public class Arkanoid extends JFrame implements KeyListener {
 	/* VARIABLES DEL JUEGO */
 
 	private boolean tryAgain = false;
+	private boolean actualizado = false;
 	private boolean running = false;
 	private boolean paused = false;
+	private boolean dificil = false;
+	private boolean augVelocidad = false;
+
 
 	private Paddle paddle = new Paddle(ANCHURA_PANTALLA / 2, ALTURA_PANTALLA - 50);
 	private Ball ball = new Ball(ANCHURA_PANTALLA / 2, ALTURA_PANTALLA / 2);
 	private List<Brick> bricks = new ArrayList<Arkanoid.Brick>();
 	private ScoreBoard scoreboard = new ScoreBoard();
+	private reproducirSonido sonidoambiente = new reproducirSonido();
 
 	private double lastFt;
 	private double currentSlice;
@@ -81,7 +87,6 @@ public class Arkanoid extends JFrame implements KeyListener {
 	class ScoreBoard {
 
 		int score = 0;
-		int score_lvl1 = 0;
 		int lives = VIDAS_JUGADOR;
 		boolean win = false;
 		boolean gameOver = false;
@@ -97,10 +102,21 @@ public class Arkanoid extends JFrame implements KeyListener {
 		void increaseScore() {
 
 			score++;
-			
-			System.out.println(" ");
-			
-			if (bricks.size() == 1) {
+
+			if (bricks.size() == 1 && nivel == 0) {
+				nivel = 1;
+				win = true;
+				text = "";
+
+			} else if (bricks.size() == 1 && nivel == 1) {
+				nivel = 2;
+				win = true;
+				text = "";
+			} else if (bricks.size() == 1 && nivel == 2){
+				nivel = 3;
+			}
+
+			if (bricks.size() == 1 && nivel == 3) {
 
 				reproducirSonido sonido = new reproducirSonido();
 
@@ -111,8 +127,14 @@ public class Arkanoid extends JFrame implements KeyListener {
 					System.out.println(" ");
 				}
 
+				try {
+					sonidoambiente.Stop();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
 				win = true;
-				text = "¡Has ganado!" + SL  + "Puntuacion: " + (score+score_lvl1)
+				text = "¡Has ganado!" + SL  + "Puntuacion: " + (score)
 						+ SL + SL +"Apreta Enter" + SL + "para reiniciar";
 			} else {
 				updateScoreboard();
@@ -121,20 +143,18 @@ public class Arkanoid extends JFrame implements KeyListener {
 
 		void die() {
 
-			reproducirSonido sonido = new reproducirSonido();
-
-			try {
-				sonido.AbrirFichero(SONIDOMUERTE);
-				sonido.Play();
-			} catch (Exception e) {
-				System.out.println(" ");
-			}
-
 			lives--;
 
 			if (lives == 0) {
 				gameOver = true;
-				text = "¡Has perdido!" + SL  + "Puntuacion: " + (score+score_lvl1)
+
+				try {
+					sonidoambiente.Stop();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}				
+
+				text = "¡Has perdido!" + SL  + "Puntuacion: " + (score)
 						+ SL + SL +"Apreta Enter" + SL + "para reiniciar";
 			} else {
 				updateScoreboard();
@@ -193,7 +213,16 @@ public class Arkanoid extends JFrame implements KeyListener {
 		}
 
 		void update() {
+
 			x += velocity * FT_STEP;
+
+			if(x > 764) {
+				x = 764.2;
+			}
+
+			if(x < 36) {
+				x = 36.2;
+			}
 		}
 
 		void stopMove() {
@@ -290,10 +319,15 @@ public class Arkanoid extends JFrame implements KeyListener {
 			x += velocityX * FT_STEP;
 			y += velocityY * FT_STEP;
 
-			if (left() < 0)
+			if (left() < 0 && !augVelocidad)
 				velocityX = VELOCIDAD_BOLA;
-			else if (right() > ANCHURA_PANTALLA)
+			else if (left() < 0 && augVelocidad)
+				velocityX = VELOCIDAD_BOLA*2;
+			else if (right() > ANCHURA_PANTALLA && !augVelocidad)
 				velocityX = -VELOCIDAD_BOLA;
+			else if (right() > ANCHURA_PANTALLA && augVelocidad)
+				velocityX = -VELOCIDAD_BOLA*2;
+
 			if (top() < 0) {
 				velocityY = VELOCIDAD_BOLA;
 			} else if (bottom() > ALTURA_PANTALLA) {
@@ -301,8 +335,7 @@ public class Arkanoid extends JFrame implements KeyListener {
 				x = paddle.x;
 				y = paddle.y - 50;
 				scoreBoard.die();
-			}
-
+			}		
 		}
 
 		double left() {
@@ -332,11 +365,30 @@ public class Arkanoid extends JFrame implements KeyListener {
 
 		if (!isIntersecting(mPaddle, mBall))
 			return;
+
 		mBall.velocityY = -VELOCIDAD_BOLA;
-		if (mBall.x < mPaddle.x)
+
+		double calculo = (mBall.x - mPaddle.x);
+
+		if(calculo >= -10.0 && calculo <= 10){
+			System.out.println("EN EL CENTRO");
+			mBall.velocityX = -VELOCIDAD_BOLA*0.4;
+		} 
+		else if(calculo <= 40.0 && calculo >= 30.0){
+			System.out.println("ESQUINA DERECHA");
+			mBall.velocityX = VELOCIDAD_BOLA*2;
+			augVelocidad = true;
+		} else if(calculo <= -30.0 && calculo >= -40.0){
+			System.out.println("ESQUINA IZQUIERDA");
+			mBall.velocityX = -VELOCIDAD_BOLA*2;
+			augVelocidad = true;
+		} else if (calculo <= -11.0 && calculo >= -29.0) {
+			System.out.println("IZQUIERDA MEDIA");
+			mBall.velocityX = VELOCIDAD_BOLA;	}
+		else {
+			System.out.println("DERECHA MEDIA");
 			mBall.velocityX = -VELOCIDAD_BOLA;
-		else
-			mBall.velocityX = VELOCIDAD_BOLA;
+		}
 	}
 
 	void testCollision(Brick mBrick, Ball mBall, ScoreBoard scoreboard) {
@@ -371,9 +423,30 @@ public class Arkanoid extends JFrame implements KeyListener {
 
 		for (int iX = 0; iX < CANTIDAD_BLOQUES_X; ++iX) {
 			for (int iY = 0; iY < CANTIDAD_BLOQUES_Y; ++iY) {
-				bricks.add(new Brick((iX + 1) * (ANCHURA_BLOQUE + 3) + 22,
+				bricks.add(new Brick((iX + 1) * (ANCHURA_BLOQUE + 3) + 22/*22*/,
 						(iY + 2) * (ALTURA_BLOQUE + 3) + 30));
 			}
+		}
+
+
+		// ESTO ESCRIBE CIDE
+		if(nivel == 0) {
+
+			for(int i = 0; i < 3; i++)
+				bricks.remove(6);
+			for(int i = 0; i < 5; i++)
+				bricks.remove(7); 
+			for(int i = 0; i < 5; i++)
+				bricks.remove(12);
+			for(int i = 0; i < 3; i++)
+				bricks.remove(18);
+
+			bricks.remove(19);	bricks.remove(22);
+
+			for(int i = 0; i < 5; i++)
+				bricks.remove(22);
+
+			bricks.remove(28);	bricks.remove(29);
 		}
 	}
 
@@ -406,12 +479,27 @@ public class Arkanoid extends JFrame implements KeyListener {
 		drawScene(ball, bricks, scoreboard);
 
 		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
+			sonidoambiente.AbrirFichero(AMBIENTE);
+			sonidoambiente.Play();
+		} catch (Exception e) {
+			System.out.println("Ha fallado");
 		}
 
 		while (running) {
+
+			if(nivel == 1 && actualizado == false) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			} else if(nivel == 2 && actualizado == false) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			}
 
 			long time1 = System.currentTimeMillis();
 
@@ -428,17 +516,77 @@ public class Arkanoid extends JFrame implements KeyListener {
 				}
 
 			} else {
-				if (tryAgain) {
-					tryAgain = false;
+
+				if(nivel == 1) {
+
+					actualizado = true;
+					CANTIDAD_BLOQUES_X = 11;
+					CANTIDAD_BLOQUES_Y = 5;
+
+					if(dificil)
+						VELOCIDAD_BOLA = 0.6;
+					else
+						VELOCIDAD_BOLA = 0.5;
+					scoreboard.win = false;
+
+					initializeBricks(bricks);
+					scoreboard.updateScoreboard();
+
+					ball.x = ANCHURA_PANTALLA / 2;
+					ball.y = ALTURA_PANTALLA / 2;
+					paddle.x = ANCHURA_PANTALLA / 2;
+
+
+				} else if(nivel == 2) {
+
+					actualizado = true;
+					CANTIDAD_BLOQUES_X = 11;
+					CANTIDAD_BLOQUES_Y = 6;
+
+					if(dificil)
+						VELOCIDAD_BOLA = 0.8;
+					else
+						VELOCIDAD_BOLA = 0.6;
+					scoreboard.win = false;
+
+					scoreboard.win = false;
+
+					initializeBricks(bricks);
+					scoreboard.updateScoreboard();
+
+					ball.x = ANCHURA_PANTALLA / 2;
+					ball.y = ALTURA_PANTALLA / 2;
+					paddle.x = ANCHURA_PANTALLA / 2;
+
+
+				} else if (tryAgain) {
+
+					try {
+						sonidoambiente.Play();
+					} catch (Exception e) {
+						System.out.println("Ha fallado");
+					}
+
+					nivel = 0;
+					VELOCIDAD_BOLA = 0.4;
+					CANTIDAD_BLOQUES_X = 11;
+					CANTIDAD_BLOQUES_Y = 5;
+
 					initializeBricks(bricks);
 					scoreboard.lives = VIDAS_JUGADOR;
 					scoreboard.score = 0;
 					scoreboard.win = false;
 					scoreboard.gameOver = false;
+
 					scoreboard.updateScoreboard();
+
 					ball.x = ANCHURA_PANTALLA / 2;
 					ball.y = ALTURA_PANTALLA / 2;
 					paddle.x = ANCHURA_PANTALLA / 2;
+
+					tryAgain = false;
+					actualizado = false;
+
 				}
 				else if(paused && !scoreboard.gameOver){
 					scoreboard.paused();
@@ -483,10 +631,14 @@ public class Arkanoid extends JFrame implements KeyListener {
 				testCollision(brick, ball, scoreboard);
 				if (brick.destroyed) {
 					it.remove();
+					augVelocidad = false;
 				}
 			}
 
 		}
+
+
+
 	}
 
 	private void drawScene(Ball ball, List<Brick> bricks, ScoreBoard scoreboard) {
